17161f593a8411aa647b1a7216731b0ae0d6fb3d
Ryan Davis <ryand-ruby@zenspider.com>
2016-04-12 20:28:32 -0700

    problem 2

--- zenspider/lib/rosalind.rb
+++ zenspider/lib/rosalind.rb
@@ -1,6 +1,25 @@
 class Rosalind
+  def parse s
+    s.each_line.map { |l|
+      case l
+      when /^\s*#|^$/ then
+        next
+      else
+        l.chomp
+      end
+    }.compact.join "\n"
+  end
+
+  def count_dna dna
+    %w[A C G T].map { |nt| dna.count nt }
+  end
+
+  def dna_to_rna dna
+    dna.tr "T", "U"
+  end
+
   def run s
-    puts %w[A C G T].map { |nt| s.count nt }.join " "
+    puts dna_to_rna parse s
   end
 end
 
