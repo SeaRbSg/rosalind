c7c69d016f9040f3e108e7b3e6d2c1577978a8a3
Ryan Davis <ryand-ruby@zenspider.com>
2016-04-12 21:15:24 -0700

    GC problem

--- zenspider/lib/rosalind.rb
+++ zenspider/lib/rosalind.rb
@@ -27,6 +27,22 @@ class Rosalind
     reverse_compliment s
   end
 
+  def cmd_gc s
+    gc fasta s
+  end
+
+  def gc fasta
+    fasta.map { |k, v| [k, gc_content(v)] }.max_by { |k,v| v }
+  end
+
+  def gc_content dna
+    100.0 * dna.delete("AT").size / dna.size
+  end
+
+  def fasta s
+    Hash[*s.split(/^>(\w+)\n/).drop(1).map { |ss| ss.delete("\n") }]
+  end
+
   def count_dna dna
     %w[A C G T].map { |nt| dna.count nt }
   end
