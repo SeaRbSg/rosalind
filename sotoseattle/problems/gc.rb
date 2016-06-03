addc90dfaccc781461cad89b297a48422c954879
Javier Soto <sotoseattle@gmail.com>
2016-06-03 11:05:37 -0700

    refactor gc

--- sotoseattle/problems/gc.rb
+++ sotoseattle/problems/gc.rb
@@ -2,17 +2,14 @@ require_relative './revc'
 
 class Rosalind
 
-  ## GC content of dna strand
-
-  def self.gc_content dna_str
-    100.0 * dna_str.count('GC') / dna_str.size
+  ## gc exercise == pick the label of the dna strand with highest GC content
+  def self.gc dna_str
+    fasta(dna_str).map{ |lab, str| [lab, self.gc_content(str)] }.max_by &:last
   end
 
-  ## Pick the label of the dna strand with highest GC content
-
-  def self.gc input_dna
-    input_dna.map { |a| [a.first, self.gc_content(a.last)] }
-             .max { |a| a.last }
+  ## GC content of dna strand
+  def self.gc_content dna_str
+    100.0 * dna_str.count('GC') / dna_str.length
   end
 
 end
