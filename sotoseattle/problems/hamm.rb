c7d32bf23f0414780615e930eadafac5ddbe4b0d
Javier Soto <sotoseattle@gmail.com>
2016-06-03 10:16:34 -0700

    refactor hamm

--- sotoseattle/problems/hamm.rb
+++ sotoseattle/problems/hamm.rb
@@ -2,10 +2,13 @@ require_relative './revc'
 
 class Rosalind
 
-  ## compare two strands nucleotid by nucleotid and count differences
-
+  ## hamm exercise == compare two strands to count nucleic acid differences
   def self.hamm s, t
     s.chars.zip(t.chars).count { |a| a.uniq.size == 2 }
+  end
+
+  class << self
+    alias_method :hamming_distance, :hamm
   end
 
 end
