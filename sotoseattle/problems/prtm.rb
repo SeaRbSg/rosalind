6915a631b4de53680d3bc5997cecd9822d716142
Javier Soto <sotoseattle@gmail.com>
2016-06-03 12:14:09 -0700

    refactor prtm

--- sotoseattle/problems/prtm.rb
+++ sotoseattle/problems/prtm.rb
@@ -2,8 +2,13 @@ require_relative './prot'
 
 class Rosalind
 
+  ## prtm exercise == total weight of a protein
   def self.prtm protein_strand
     protein_strand.chomp.chars.map{ |amino| AMINO_MASS[amino] }.reduce :+
+  end
+
+  class << self
+    alias_method :protein_weight, :prtm
   end
 
 end
