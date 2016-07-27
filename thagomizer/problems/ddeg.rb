910d4d3e5c025ad15e2a330ce0dabc76ae61594c
Aja Hammerly <aja.hammerly+github@gmail.com>
2016-07-26 18:31:41 -0700

    ddeg

---
+++ thagomizer/problems/ddeg.rb
@@ -0,0 +1,33 @@
+# Problem
+
+# Given:
+# A simple graph with n <= 10^3 vertices in the edge list format.
+
+# Return:
+# An array D[1..n] where D[i] is the sum of the degrees of i's neighbors.
+
+# Sample Dataset
+# 5 4
+# 1 2
+# 2 3
+# 4 3
+# 2 4
+
+# Sample Output
+# 3 5 5 5 0
+
+
+require 'pp'
+
+edges = File.read("ddeg.txt").split("\n").map { |l| l.split(/\s/).map(&:to_i) }
+max, _ = edges.shift
+
+neighbors = Hash.new { |h,k| h[k] = [] }
+
+edges.each do |from, to|
+  neighbors[from] << to
+  neighbors[to]   << from
+end
+
+puts (1..max).map { |k| neighbors[k].inject(0) { |memo, i| memo + neighbors[i].length } }.join(' ')
+
