d59e1a3aa50f6770158dfdf25dbf01fab5779ba3
Aja Hammerly <aja.hammerly+github@gmail.com>
2016-08-26 16:58:27 -0700

    Par3

---
+++ thagomizer/problems/par3.rb
@@ -0,0 +1,28 @@
+# Problem
+
+# This problem is very similar to "2-Way Partition", but now the goal
+# is to partition an input array more carefully.
+
+
+# Given: A positive integer n <= 10^5 and an array A[1..n] of integers
+# from -10^5 to 10^5.
+
+# Return: An array B[1..n] such that it is a permutation of A and
+# there are indices 1 <= q <= r <= n such that B[i]<A[1] for all
+# 1 <= i <= q-1, B[i]=A[1] for all q <= i <= r, and B[i]>A[1] for all
+# r+1 <= i <= n.
+
+# Sample Dataset
+# 9
+# 4 5 6 4 1 2 5 7 4
+
+# Sample Output
+# 2 1 4 4 4 5 7 6 5
+
+require 'pp'
+
+ary = File.read("par3.txt").split(/\n/)[1].split(/\s/).map(&:to_i)
+
+groups = ary.group_by { |x| x <=> ary[0] }
+
+puts "#{groups[-1].join(' ')} #{groups[0].join(' ')} #{groups[1].join(' ')}"
