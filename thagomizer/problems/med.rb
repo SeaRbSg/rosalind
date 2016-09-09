6e59d2a6e59f15dff43414174b0b6c48d961ddd6
Aja Hammerly <aja.hammerly@gmail.com>
2016-09-09 19:41:50 +0900

    MED

---
+++ thagomizer/problems/med.rb
@@ -0,0 +1,45 @@
+# Problem
+# The task is to implement a linear time randomized algorithm for the
+# selection problem.
+
+# Given: A positive integer n <= 10^5 and an array A[1..n] of integers
+# from -10^5 to 105, a positive number k <= n.
+
+# Return:
+# The k-th smallest element of A.
+
+# Sample Dataset
+# 11
+# 2 36 5 21 8 13 11 20 5 4 1
+# 8
+
+# Sample Output
+# 13
+
+data = File.read("med.txt").split("\n")
+
+_   = data.shift
+ary = data.shift.split(" ").map(&:to_i)
+k   = data.shift.to_i
+
+loop do
+  r = ary[Random.rand(0...ary.length)]
+
+  groups = ary.group_by { |i| i <=> r}
+  s_l = groups[-1] || []
+  s_m = groups[0]  || []
+  s_h = groups[1]  || []
+
+  case
+  when k <= s_l.length
+    ary = s_l
+    next
+  when k <= s_l.length + s_m.length
+    puts s_m[0]
+    break
+  else
+    k - = s_l.length + s_m.length
+    ary = s_h
+    next
+  end
+end
