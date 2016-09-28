543666b43c5fd03b8cf81f46a588f94166218a2e
Aja Hammerly <aja.hammerly@gmail.com>
2016-09-27 18:34:41 -0700

    WIP

--- thagomizer/lib/heap.rb
+++ thagomizer/lib/heap.rb
@@ -1,4 +1,15 @@
 class Heap
+  def self.heapify ary
+    h = Heap.new
+    h.data = ary
+
+    # Bubble Up
+    (h.data.length - 1).downto(0) do |i|
+      
+
+    end
+  end
+
   def initialize
     @data       = []
     @next_index = 1
@@ -14,7 +25,6 @@ class Heap
     # look at children 2i + 1 and 2i + 2
     # take the larger of the children, if it is less that @data[i] done
     # otherwise swap the larger child with the root and repeat
-
   end
 
   def insert x
